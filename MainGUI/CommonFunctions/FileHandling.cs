5b3e0cbdbe88c3badb0c46da3b29e7fb10ddf423
tom-oliver-ward <37127495+tom-oliver-ward@users.noreply.github.com>
2018-07-19 09:21:57 +1200

    Starting FileHandling

---
+++ MainGUI/CommonFunctions/FileHandling.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonFunctions
+{
+    /// <summary>
+    /// Manages reading in files for all data types
+    /// </summary>
+    public class FileHandling
+    {
+        /// <summary>
+        /// Reads the text in the given file
+        /// </summary>
+        /// <param name="filePath">path of the file to read</param>
+        /// <returns>the text in the given file</returns>
+        public string ReadFile(string filePath)
+        {
+            //variable to store data read
+            string text = File.ReadAllText(filePath);            
+
+            return text;            
+        }
+    }
+}
---
+++ MainGUI/CommonTests/FileHandlingTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonTests
+{
+    [TestClass]
+    public class FileHandlingTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
