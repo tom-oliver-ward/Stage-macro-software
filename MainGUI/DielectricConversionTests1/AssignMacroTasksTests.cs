d509494ca86a40d441cbc816eab31289531ce8d2
tom-oliver-ward <37127495+tom-oliver-ward@users.noreply.github.com>
2018-08-17 10:29:39 +1200

    need to plan how to unit test

---
+++ MainGUI/DielectricConversionTests1/AssignMacroTasksTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DielectricConversion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DielectricConversionTests1
+{
+    [TestClass()]
+    public class AssignMacroTasksTests
+    {
+        [TestMethod()]
+        public void MoveRelativeTest()
+        {
+            //arrange
+            var dielectricConversion = new AssignMacroTasks();
+            var currentFile = new CommonFunctions.FileHandling();
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\macrotest.demcr";
+
+            //act
+            var rawData = currentFile.ReadFile(filepath);
+            dielectricConversion.MoveRelative()
+
+            //assert
+            Assert.Fail();
+        }
+    }
+}
